844f92cb828656bdb1784379f0bbfcf62df7499c
admin <admin@ad.local>
2016-02-25 17:29:39 +0800

    NeuralNetwork Test

--- HandlewrittenDigitsRecogniton.py
+++ HandlewrittenDigitsRecogniton.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 from sklearn.datasets import load_digits
-from sklearn.metrics import confusion_matrix,classification_report
+from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.preprocessing import LabelBinarizer
 from NeuralNetwork import NeturaNetwork
 from  sklearn.cross_validation import train_test_split
@@ -8,16 +9,16 @@ from  sklearn.cross_validation import train_test_split
 digits = load_digits()
 X = digits.data
 y = digits.target
-sety = list(set(y))
-X -= X.min()
+# sety = list(set(y))
+X -= X.min()    #转化到0-1之间
 X /= X.max()
 
-nn = NeturaNetwork([X.shape[1],100,len(sety)],'logistic')
-X_train , X_test, y_train,y_test = train_test_split(X,y)
+nn = NeturaNetwork([64, 100, 10], 'logistic')
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 labels_train = LabelBinarizer().fit_transform(y_train)
 labels_test = LabelBinarizer().fit_transform(y_test)
-nn.fit(X_train,labels_train,epochs=6000)
-predictions =[]
+nn.fit(X_train, labels_train, epochs=6000)
+predictions = []
 for i in range(X_test.shape[0]):
     o = nn.predict(X_test[i])
     predictions.append(np.argmax(o))
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 
 
@@ -27,29 +28,46 @@ class NeturaNetwork():
             self.activation_deriv = tanh_deriv
 
         self.weight = []
+
         for i in range(1, len(layers) - 1):
+            #5 * np.random.random_sample((3, 2)) - 5
+            #生成一个矩阵（3x2) 值在0，1间，5*（0,1）-5 随机值 -5 生产一个（-5,0）的随机数组
+            # array([[-3.99149989, -0.52338984],
+            #        [-2.99091858, -0.79479508],
+            #        [-1.23204345, -1.75224494]])
+            #生成一个（65,101)的矩阵，多的行数用来存储bias（偏移量）
+
             self.weight.append((2 * np.random.random((layers[i - 1] + 1, layers[i] + 1)) - 1) * 0.25)
             self.weight.append((2 * np.random.random((layers[i] + 1, layers[i + 1])) - 1) * 0.25)
 
     def fit(self, X, y, learning_rate=0.2, epochs=10000):
         X = np.atleast_2d(X)
+        #生成一个（65,101)的矩阵，多的行数用来存储bias（偏移量）
         temp = np.ones([X.shape[0], X.shape[1] + 1])
         temp[:, 0:-1] = X
         X = temp
         y = np.array(y)
-
         for k in range(epochs):
             i = np.random.randint(X.shape[0])
             a = [X[i]]
-
+            #正向更新
             for l in range(len(self.weight)):
                 a.append(self.activation(np.dot(a[l], self.weight[l])))
             error = y[i] - a[-1]
             deltas = [error * self.activation_deriv(a[-1])]
 
-            # start backprobagation
+        # start backprobagation,deltas为（10，）的矩阵，计算内积需要适用weight[l].T转化 ndarray.T == numpy.ndarray.transpose
+        # >>> a
+        # array([[0, 1, 2],
+        #        [3, 4, 5]])
+        # >>> a.T
+        # array([[0, 3],
+        #        [1, 4],
+        #        [2, 5]])
             for l in range(len(a) - 2, 0, -1):
+                print self.weight[l].shape
                 deltas.append(deltas[-1].dot(self.weight[l].T) * self.activation_deriv(a[l]))
+
             deltas.reverse()
             for i in range(len(self.weight)):
                 layer = np.atleast_2d(a[i])
